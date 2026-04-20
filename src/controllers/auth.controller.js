eb35d0ce1619efdda76c40bf5528040719be088a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-20 23:15:07 +0530

    auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/user.model")
+
+
+function userRegisterController(req,res) {
+
+    const {email, password , name} = req.userModel
+
+
+    
+    
+}
+
+module.exports = {
+    userRegisterController
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,9 +2,11 @@ const express = require("express")
 
 const router = express.Router()
 
+const authController = require("../controllers/auth.controller")
+
 
 // POST /api/auth/register
-router.post("/register")
+router.post("/register", authController.userRegisterController)
 
 
 
