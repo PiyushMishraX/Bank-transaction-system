881187e2c98d7a6ad9c55d49148bc6be5ac0f54b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-23 22:14:56 +0530

    send email feature completed

--- src/config/db.js
+++ src/config/db.js
@@ -16,14 +16,4 @@ function connectToDB(){
     })
 }
 
-async function sendRegistrationEmail(userEmail, name){
-    const subject = 'Welcome to Backend Ledger!';
-    const text = `Hello ${name},\n\nThank you for registering at Backend Ledger. We're excited to have you on board!,\n\nBest regards,\nThe Backend Ledger team`;
-    const html = `<p>Hello ${name},</p><p>Thank you for registering at Backend Ledger. We're excited to have you on board!</p><p>Best regards,<br>The Backend Ledger team</p>`;
-
-    await sendEmail(userEmail, subject, text , html);
-}
-
-module.exports = {
-    sendRegistrationEmail
-};
+module.exports = connectToDB;
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -41,4 +41,14 @@ const sendEmail = async (to, subject, text, html) => {
   }
 };
 
-module.exports = sendEmail;
+async function sendRegistrationEmail(userEmail, name){
+    const subject = 'Welcome to Backend Ledger!';
+    const text = `Hello ${name},\n\nThank you for registering at Backend Ledger. We're excited to have you on board!,\n\nBest regards,\nThe Backend Ledger team`;
+    const html = `<p>Hello ${name},</p><p>Thank you for registering at Backend Ledger. We're excited to have you on board!</p><p>Best regards,<br>The Backend Ledger team</p>`;
+
+    await sendEmail(userEmail, subject, text , html);
+}
+
+module.exports = {
+    sendRegistrationEmail
+};
