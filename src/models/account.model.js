cf964303e5d59d4a614561f70a88ac6a7633c5a3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-23 22:48:21 +0530

    acc status and currency type

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -4,9 +4,22 @@ const mongoose = require("mongoose")
 
 const accountSchema = new mongoose.Schema({
     user:{
-        type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId, // here we enter the objectid of the usermodel
         ref: "user", // name of usermodel,
         required: [true, "Account must be associated with a user"]
 
     },
+    status: { // account uis working , froxen or closed
+        enum: {
+            value: [ "ACTIVE", "FROZEN", "CLOSED"],
+            message: "Status can be either ACTIVE, FROZEN OR CLOSED" 
+        }
+    },
+    currency: {
+        type: String,
+        required: [true, "Currency is required for creating an account"],
+        default: "INR"
+    },
+    
+
 })
