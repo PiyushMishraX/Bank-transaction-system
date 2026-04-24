024465974af6f02b1addb8dfe746ce93c789c455
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-24 18:09:18 +0530

    remove stopping problem of authMiddleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken")
 
 async function authMiddleware(req, res, next) {
 
+    // console.log("authMidd 1");
+    
+
     const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
     // The Authorization header is a standard HTTP header used to carry credentials (like a Bearer Token) to authenticate a client with a server
 
@@ -10,25 +13,32 @@ async function authMiddleware(req, res, next) {
         return res.status(401).json({
             message: "Unauthorized access, token is missing"
         })
-
-        try {
-
-            const decodedd = jwt.verify(token, process.env.JWT_SECRET)
-
-            const user = await userModel.findById(decodedd.userId)
-
-            req.user = user // request now have user which we can use in the controller after the next()
-
-            return next() 
-            
-        } catch (err) {
-
-            return res.status(401).json({
-                message: "unauthorized access, token is invalid"
-            })
-            
-        }
     }
+
+    try {
+
+        // console.log("authmiddleware");
+        
+
+        const decodedd = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decodedd.userId)
+
+        req.user = user // request now have user which we can use in the controller after the next()
+
+        return next() 
+        
+    } catch (err) {
+
+        // console.log("authMidd err");
+        
+
+        return res.status(401).json({
+            message: "unauthorized access, token is invalid"
+        })
+        
+    }
+    
     
 }
 
