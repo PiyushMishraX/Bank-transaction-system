3902c0080063d2f4856b7b3694cb1e77e6e01cb2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-26 20:22:13 +0530

    add immutability in ledger fields

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -6,16 +6,19 @@ const ledgerSchema = new mongoose.Schema({
         ref: "account",
         required: [ true, "Ledger must be associated with an account"],
         index: true,
+        immutable: true // no change , because a ledger entry cannot be changed after creation bcz it is the true value
     },
     amount: {
         type: Number,
         required: [ true, "Amount is reqired for crating a ledger entry"],
+        immutable: true
     },
     transaction: { // Which transaction is being done will be added in the ledger
         type: mongoose.Schema.Types.ObjectId,
         ref: "transaction",
         reuqired: [ true, "Ledger must be associated with a transaction"],
         index: true,
+        immutable: true
     },
 
 })
