a2cb57278212e201d98b74f93b5171dff5af6054
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-26 22:04:34 +0530

    create transaction route

--- src/app.js
+++ src/app.js
@@ -13,12 +13,14 @@ app.use(cookieParser())
  */
 const authRouter = require("./routes/auth.routes")
 const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
 
 /**
  * - use Routes
  */
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
 
 
 module.exports = app;
