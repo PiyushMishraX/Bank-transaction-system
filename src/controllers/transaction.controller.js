36344d994b8c56d439615a2aeac9b83e3715b9ad
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-03 20:49:43 +0530

    bug fixes

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -58,26 +58,26 @@ async function createTransaction(req, res){
     })
 
     if(isTransactionAlreadyExists){
-        if(isTransactionAlreadyExists.status == "COMPLETED") {
+        if(isTransactionAlreadyExists.status === "COMPLETED") {
             return res.status(200).json({
                 message: "Transaction already processed",
                 transaction: isTransactionAlreadyExists
             })
         }
 
-        if(isTransactionAlreadyExists == "PENDING"){
+        if(isTransactionAlreadyExists.status === "PENDING"){
             return res.status(200).json({
                 message: "Transaction is still in processing",
             })
         }
 
-        if(isTransactionAlreadyExists == "FAILED"){
+        if(isTransactionAlreadyExists.status === "FAILED"){
             return res.status(500).json({
                 message: "Transaction processing failed,please retry"
             })
         }
 
-        if(isTransactionAlreadyExists == "REVERSED"){
+        if(isTransactionAlreadyExists.status === "REVERSED"){
             return res.status(200).json({
                 message: "Transaction was reversed, please retry",
             })
@@ -89,9 +89,9 @@ async function createTransaction(req, res){
      * 3. Check account status
      */
 
-    if(fromUserAccount.status !== "ACTIVE" || toUserAccount !== "ACTIVE"){
+    if(fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
         return res.status(400).json({
-            message: "Both fromAccount and toAccount must be ACTIVE to process transactio "
+            message: "Both fromAccount and toAccount must be ACTIVE to process transaction"
         })
     }
 
@@ -117,12 +117,13 @@ async function createTransaction(req, res){
     session.startTransaction()
 
     // create({}, {session})
-    const transaction = await transactionModel.create({
+    const transaction = new transactionModel({
         fromAccount,
         toAccount,
+        amount,
         idempotencyKey,
         status: "PENDING"
-    }, { session })
+    })
 
     /**
      *  6. Create DEBIT ledger entry
@@ -130,24 +131,24 @@ async function createTransaction(req, res){
 
     // money going -> debit
     // money coming -> credit
-    const debitLedgerEntry = await ledgerModel.create({
+    const debitLedgerEntry = await ledgerModel.create( [{
         account: fromAccount,
         amount: amount,
         transaction: transaction._id,
         type: "DEBIT"
-    }, { session })
+    }]  , { session })
     
     
 
     /**
      * 7. Create CREDIT ledger entry
      */
-    const creditLedgerEntry = await ledgerModel.create({
+    const creditLedgerEntry = await ledgerModel.create( [{
         account: toAccount,
         amount: amount,
         transaction: transaction._id,
         type: "CREDIT"
-    }, { session })
+    }]  , { session })
 
     /**
      * 8. Mark transaction COMPLETED
@@ -161,7 +162,7 @@ async function createTransaction(req, res){
      */
 
     await session.commitTransaction()
-    session.endSession
+    session.endSession()
 
 
     /**
@@ -177,8 +178,6 @@ async function createTransaction(req, res){
     })
 
         
-
-
 }
 
 async function createInitialFundsTransaction(req, res){
