60555be984e34f427efb768a002187ddebd53fac
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-04 00:00:45 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ const transactionRoutes = require("./routes/transaction.routes")
 /**
  * - use Routes
  */
+
+app.get("/",(req, res)=> {
+    res.send("Ledger Service is up and running") // demo api to test if server is running in depolyment, and isn't down
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRoutes)
